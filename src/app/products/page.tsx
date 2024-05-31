32ace197dc63762271af71853eda8a8e729d0b8e
LeNghiaaa <168074726+LeNghiaaa@users.noreply.github.com>
2024-06-01 00:11:31 +0700

    Implement Product page items

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+
+import product_hero_images from "../../../public/images/productImages/product_hero_image.png";
+import product_preview_1 from "../../../public/images/productImages/product_preview_1.png";
+import product_preview_2 from "../../../public/images/productImages/product_preview_2.png";
+import product_preview_3 from "../../../public/images/productImages/product_preview_3.png";
+import product_preview_4 from "../../../public/images/productImages/product_preview_4.png";
+import UserBreadcrumb from "@/components/shared/breadcrumb";
+import { PRODUCT_DATA } from "@/constants/product.data";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const ProductPage = () => {
   return (
-    <div>
-      Product Page
-    </div>
-  )
-}
+    <main className="text-[#C6613D]">
+      <Image
+        src={product_hero_images}
+        alt="candleHeroImage"
+        className="w-full"
+      />
+      <div className="mt-[25px]">
+        <UserBreadcrumb />
+      </div>
+      <div className="flex gap-[60px] mt-[40px] justify-center">
+        {PRODUCT_DATA.map(({ id, name }) => (
+          <div
+            className="cursor-pointer text-[20px] font-medium border-b-2 border-[#C6613D] leading-6"
+            key={id}
+          >
+            {name}
+          </div>
+        ))}
+      </div>
+      <Carousel className="flex justify-center">
+        <CarouselContent className="mt-[50px]">
+          <CarouselItem className="max-w-[200px] max-h-[400px]">
+            <Image src={product_preview_1} alt="product_preview_1" />
+          </CarouselItem>
+          <CarouselItem className="max-w-[200px] max-h-[400px]">
+            <Image src={product_preview_2} alt="product_preview_1" />
+          </CarouselItem>
+          <CarouselItem className="max-w-[200px] max-h-[400px]">
+            <Image src={product_preview_3} alt="product_preview_1" />
+          </CarouselItem>
+          <CarouselItem className="max-w-[200px] max-h-[400px]">
+            <Image src={product_preview_4} alt="product_preview_1" />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselNext />
+        <CarouselPrevious />
+      </Carousel>
+      <div className="w-full h-[1px] bg-[#C6613D] mt-[60px] mb-8"></div>
+    </main>
+  );
+};
 
-export default ProductPage
+export default ProductPage;
---
+++ src/constants/product.data.ts
@@ -0,0 +1,6 @@
+export const PRODUCT_DATA = [
+  { id: 1, name: "Nến thơm", href: "" },
+  { id: 1, name: "Sáp Thơm", href: "" },
+  { id: 1, name: "Room Sprays", href: "" },
+  { id: 1, name: "Khuếch tán tinh dầu", href: "" },
+];
