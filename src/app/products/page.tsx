b5033f473cb6aa94fd157cd8761dd88565d0c2b7
LeNghiaaa <168074726+LeNghiaaa@users.noreply.github.com>
2024-06-01 19:33:06 +0700

    Fix Product page as new design

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -6,6 +6,9 @@ import product_preview_1 from "../../../public/images/productImages/product_prev
 import product_preview_2 from "../../../public/images/productImages/product_preview_2.png";
 import product_preview_3 from "../../../public/images/productImages/product_preview_3.png";
 import product_preview_4 from "../../../public/images/productImages/product_preview_4.png";
+import product_carousel_1 from "../../../public/images/productImages/product_carousel_1.png";
+import product_carousel_2 from "../../../public/images/productImages/product_carousel_2.png";
+import product_carousel_3 from "../../../public/images/productImages/product_carousel_3.png";
 import UserBreadcrumb from "@/components/shared/breadcrumb";
 import { PRODUCT_DATA } from "@/constants/product.data";
 import {
@@ -37,25 +40,55 @@ const ProductPage = () => {
           </div>
         ))}
       </div>
-      <Carousel className="flex justify-center">
-        <CarouselContent className="mt-[50px]">
-          <CarouselItem className="max-w-[200px] max-h-[400px]">
-            <Image src={product_preview_1} alt="product_preview_1" />
+      <div className="flex justify-center gap-4 mt-[18px]">
+        <Image
+          className="max-w-[200px] max-h-[400px]"
+          src={product_preview_1}
+          alt="product_preview_1"
+        />
+        <Image
+          className="max-w-[200px] max-h-[400px]"
+          src={product_preview_2}
+          alt="product_preview_2"
+        />
+        <Image
+          className="max-w-[200px] max-h-[400px]"
+          src={product_preview_3}
+          alt="product_preview_3"
+        />
+        <Image
+          className="max-w-[200px] max-h-[400px]"
+          src={product_preview_4}
+          alt="product_preview_4"
+        />
+      </div>
+
+      <div className="w-full h-[1px] bg-[#C6613D] mt-[60px] mb-5"></div>
+      <div className="flex flex-col items-center">
+        <div>Sản phẩm bán chạy</div>
+        <div className="font-medium">Được yêu thích trong tháng</div>
+      </div>
+      <Carousel className="flex justify-center mb-10">
+        <CarouselContent className="mt-[20px]">
+          <CarouselItem className="max-w-[280px] max-h-[400px]">
+            <Image src={product_carousel_1} alt="product_preview_1" />
           </CarouselItem>
-          <CarouselItem className="max-w-[200px] max-h-[400px]">
-            <Image src={product_preview_2} alt="product_preview_1" />
+          <CarouselItem className="max-w-[280px] max-h-[400px]">
+            <Image src={product_carousel_2} alt="product_preview_1" />
           </CarouselItem>
-          <CarouselItem className="max-w-[200px] max-h-[400px]">
-            <Image src={product_preview_3} alt="product_preview_1" />
+          <CarouselItem className="max-w-[280px] max-h-[400px]">
+            <Image src={product_carousel_3} alt="product_preview_1" />
           </CarouselItem>
-          <CarouselItem className="max-w-[200px] max-h-[400px]">
-            <Image src={product_preview_4} alt="product_preview_1" />
+          <CarouselItem className="max-w-[280px] max-h-[400px]">
+            <Image src={product_carousel_3} alt="product_preview_1" />
+          </CarouselItem>
+          <CarouselItem className="max-w-[280px] max-h-[400px]">
+            <Image src={product_carousel_3} alt="product_preview_1" />
           </CarouselItem>
         </CarouselContent>
         <CarouselNext />
         <CarouselPrevious />
       </Carousel>
-      <div className="w-full h-[1px] bg-[#C6613D] mt-[60px] mb-8"></div>
     </main>
   );
 };
