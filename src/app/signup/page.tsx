35f48817efa1c895a83bcfc42707e3154e7f5958
NguyenTruongKhangThuan <ntkthuan1209@gmail.com>
2024-06-02 20:28:40 +0700

    Add Login/Signup, Setup Cart Page UI

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { Checkbox } from "@/components/ui/checkbox";
+
+import ProductBackground from '../../../public/images/ProductBackground.png';
+
+import React from "react";
+import Link from "next/link";
 
 export default function Signup() {
-    return ( 
-        <div>
-            <h1>Signup</h1>
-        </div>
-     );
+    return (
+        <section className="text-[#C6613D]">
+            <h1 className="text-[36px] font-bold uppercase py-2">Đăng ký</h1>
+            <h4 className="text-[24px] font-bold">Chào mừng bạn, vui lòng đăng ký để tiếp tục</h4>
+            <section className="flex justify-between items-center gap-4">
+                <section className="w-[560px]">
+                    <form >
+                        <div className="py-2">
+                            <Label htmlFor="email" className="pb-2">Email</Label>
+                            <Input type="email" id="email" name="email" placeholder="Vui lòng nhập email của bạn" className="rounded-[4px]"/>
+                        </div>
+                        <div className="py-2">
+                            <Label htmlFor="password" className="pb-2">Mật khẩu</Label>
+                            <Input type="password" id="password" name="password" placeholder="Vui lòng nhập mật khẩu của bạn" className="rounded-[4px]"/>
+                        </div>
+                        <div className="py-2">
+                            <Label htmlFor="confirm-password" className="pb-2">Xác nhận mật khẩu</Label>
+                            <Input type="password" id="confirm-password" name="confirm-password" placeholder="Vui lòng xác nhận mật khẩu của bạn" className="rounded-[4px]"/>
+                        </div>
+                        <Button type="button" className="bg-[#C6613D] w-full text-white uppercase font-bold text-[16px] rounded-[4px] hover:bg-[#a24f31]">Đăng ký</Button>
+                    </form>
+                    <div className="py-2">
+                        <span className="font-medium flex justify-center items-center gap-2 py-2 text-[12px]">
+                            Bạn đã có tài khoản? <Link href="/login" className="text-[#C6613D] hover:underline hover:font-bold">Đăng nhập ngay</Link>
+                        </span>
+                    </div>
+                </section>
+                <Image src={ProductBackground} alt="Product Background" className="object-cover w-[560px] h-full rounded-[4px]"/>
+            </section>
+        </section>
+    )
 }
--- src/app/user/cart/page.tsx
+++ src/app/user/cart/page.tsx
@@ -1,8 +1,67 @@
+import UserBreadcrumb from "@/components/shared/breadcrumb";
+import Link from "next/link";
+
 function Cart() {
     return ( 
-        <div>
-            <h1>Cart</h1>
-        </div>
+        <section>
+            <section className='flex justify-between items-end relative mt-12'>
+                    <hr className='w-full border-t-[3px] border-[#C6613D]' />
+                    <div className='absolute left-0 py-2 text-[#C6613D]'><UserBreadcrumb /></div>
+                    <h4 className='text-[24px] font-semibold uppercase text-[#C6613D] absolute right-0'>Giỏ hàng</h4>
+            </section>
+            <section className='flex flex-col justify-center items-center h-[80vh]'>
+                {/* Empty Cart Situation */}
+                {/* <div className='flex flex-col justify-center items-center gap-4'>
+                    <h4 className='text-[#C6613D] text-[24px] font-bold'>Giỏ hàng của bạn đang trống</h4>
+                    <Link href='/products'>
+                        <button className='bg-[#C6613D] hover:bg-[#8d442a] text-white transition-colors ease-in-out duration-500 w-[200px] h-[40px] rounded-[8px]'>Mua sắm ngay</button>
+                    </Link>
+                </div> */}
+    
+                {/* Cart Has Item Situation */}
+                <div className='flex flex-col justify-center items-center gap-4'>
+                    <h4 className='text-[#C6613D] text-[24px] font-bold'>Giỏ hàng của bạn</h4>
+                    <div className='flex flex-col gap-4'>
+                        <div className='flex justify-between items-center w-[560px]'>
+                            <div className='flex gap-4'>
+                                <img src='/images/Product_Image.png' alt='Product Image' className='w-[100px] h-[100px] object-cover rounded-[8px]' />
+                                <div className='flex flex-col gap-2'>
+                                    <h4 className='text-[#C6613D] text-[24px] font-semibold'>Sample Product</h4>
+                                    <p className='text-[#C6613D] text-[16px] font-semibold'>1000000 VND</p>
+                                </div>
+                            </div>
+                            <div className='flex gap-4'>
+                                <button className='w-[40px] h-[40px] bg-[#C6613D] hover:bg-[#8d442a] text-white transition-colors ease-in-out duration-500 rounded-[8px]'>-</button>
+                                <span className='text-[#C6613D] text-[16px] font-semibold'>1</span>
+                                <button className='w-[40px] h-[40px] bg-[#C6613D] hover:bg-[#8d442a] text-white transition-colors ease-in-out duration-500 rounded-[8px]'>+</button>
+                            </div>
+                        </div>
+                        <div className='flex justify-between items-center w-[560px]'>
+                            <div className='flex gap-4'>
+                                <img src='/images/Product_Image.png' alt='Product Image' className='w-[100px] h-[100px] object-cover rounded-[8px]' />
+                                <div className='flex flex-col gap-2'>
+                                    <h4 className='text-[#C6613D] text-[24px] font-semibold'>Sample Product</h4>
+                                    <p className='text-[#C6613D] text-[16px] font-semibold'>1000000 VND</p>
+                                </div>
+                            </div>
+                            <div className='flex gap-4'>
+                                <button className='w-[40px] h-[40px] bg-[#C6613D] hover:bg-[#8d442a] text-white transition-colors ease-in-out duration-500 rounded-[8px]'>-</button>
+                                <span className='text-[#C6613D] text-[16px] font-semibold'>1</span>
+                                <button className='w-[40px] h-[40px] bg-[#C6613D] hover:bg-[#8d442a] text-white transition-colors ease-in-out duration-500 rounded-[8px]'>+</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex justify-between w-[560px]'>
+                        <h4 className='text-[#C6613D] text-[24px] font-semibold'>Tổng cộng:</h4>
+                        <p className='text-[#C6613D] text-[24px] font-semibold'>2000000 VND</p>
+                    </div>
+                    <div className='flex justify-between w-[560px]'>
+                        <button className='w-[200px] h-[40px] bg-[#C6613D] hover:bg-[#8d442a] text-white transition-colors ease-in-out duration-500 rounded-[8px]'>Thanh toán</button>
+                        <button className='w-[200px] h-[40px] bg-none hover:bg-[#C6613D] border-[1px] border-[#C6613D] text-[#C6613D] hover:text-white transition-all ease-in-out duration-300 rounded-[8px]'>Xóa giỏ hàng</button>
+                    </div>
+                </div>
+            </section>
+        </section>
      );
 }
 
