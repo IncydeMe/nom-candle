43e97ebc75b3b6e6b4b4cbff34a32db04fe079b5
LeNghiaaa <168074726+LeNghiaaa@users.noreply.github.com>
2024-06-04 23:19:36 +0700

    Fix Header minor issue

--- src/components/shared/user-header.tsx
+++ src/components/shared/user-header.tsx
@@ -1,57 +1,89 @@
-"use client"
+"use client";
 
-import Link from 'next/link';
-import Image from 'next/image';
-import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
 
-import { CircleUserRound } from 'lucide-react';
-import { ShoppingCart } from 'lucide-react';
+import { CircleUserRound } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
 
-import Logo from '../../../public/images/sample_logo.png';
+import Logo from "../../../public/images/sample_logo.png";
 
 const UserHeader = () => {
-    return (
-        <>
-            <header className='flex w-screen h-fit justify-between px-10 items-center relative before:absolute before:left-[90px] before:bottom-0 before:h-[1px] before:w-[90%] before:border-[0.05rem] before:border-[#C6613D]'>
-                {/* Logo */}
-                <section>
-                    <Image src={Logo} alt='Logo' className='h-[120px] w-[120px] object-contain' />
-                </section>
-                {/* General Navigation */}
-                <nav>
-                    <ul className='flex justify-around gap-x-10 text-[#C6613D] '>
-                        <li>
-                            <Link href='../about' className='transition-all ease-in-out duration-300 hover:text-[#a25032] hover:font-bold hover:underline'>Về chúng tôi</Link>
-                        </li>
-                        <li>
-                            <Link href='../products' className='transition-all ease-in-out duration-300 hover:text-[#a25032] hover:font-bold hover:underline'>Sản phẩm</Link>
-                        </li>
-                        <li>
-                            <Link href='../blog' className='transition-all ease-in-out duration-300 hover:text-[#a25032] hover:font-bold hover:underline'>Blog</Link>
-                        </li>
-                        <li>
-                            <Link href='../contact' className='transition-all ease-in-out duration-300 hover:text-[#a25032] hover:font-bold hover:underline'>Liên hệ</Link>
-                        </li>
-                    </ul>
-                </nav>
-                {/* User Navigation */}
-                <nav>
-                    <ul className="flex justify-around gap-x-6 text-[#C6613D]">
-                        <li>
-                            <Link href='../user/cart' className='transition-all ease-in-out duration-300 hover:text-[#a25032] hover:font-bold hover:underline'>
-                                <ShoppingCart size={24} />
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href='../user/profile' className='transition-all ease-in-out duration-300 hover:text-[#a25032] hover:font-bold hover:underline'>
-                                <CircleUserRound size={24} />
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        </>
-    )
-}
+  return (
+    <>
+      <header className="flex w-screen h-fit justify-between px-10 items-center relative before:absolute before:left-[90px] before:bottom-0 before:h-[1px] before:w-[90%] before:border-[0.05rem] before:border-[#C6613D]">
+        {/* Logo */}
+        <section>
+          <Link href={"/"}>
+            <Image
+              src={Logo}
+              alt="Logo"
+              className="cursor-pointer h-[120px] w-[120px] object-contain"
+            />
+          </Link>
+        </section>
+        {/* General Navigation */}
+        <nav>
+          <ul className="flex justify-around gap-x-10 text-[#C6613D] ">
+            <li>
+              <Link
+                href="../about"
+                className="transition-all ease-in-out duration-300 hover:text-[#a25032] hover:font-bold hover:underline"
+              >
+                Về chúng tôi
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="../products"
+                className="transition-all ease-in-out duration-300 hover:text-[#a25032] hover:font-bold hover:underline"
+              >
+                Sản phẩm
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="../blog"
+                className="transition-all ease-in-out duration-300 hover:text-[#a25032] hover:font-bold hover:underline"
+              >
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="../contact"
+                className="transition-all ease-in-out duration-300 hover:text-[#a25032] hover:font-bold hover:underline"
+              >
+                Liên hệ
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        {/* User Navigation */}
+        <nav>
+          <ul className="flex justify-around gap-x-6 text-[#C6613D]">
+            <li>
+              <Link
+                href="../user/cart"
+                className="transition-all ease-in-out duration-300 hover:text-[#a25032] hover:font-bold hover:underline"
+              >
+                <ShoppingCart size={24} />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="../user/profile"
+                className="transition-all ease-in-out duration-300 hover:text-[#a25032] hover:font-bold hover:underline"
+              >
+                <CircleUserRound size={24} />
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
 
 export default UserHeader;
